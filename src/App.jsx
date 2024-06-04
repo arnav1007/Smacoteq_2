54404b4e6658e885aa66accb9edf6b93b65c9818
16gsanket <16gsanket@gmail.com>
2024-06-04 13:12:52 +0530

    configure the raw file for tailwindscd with plugin and all

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <div>
-      <h1 className='bg-red-400'>Hari Om</h1>
-      
+      <h1 className="m-10 bg-red-400 p-3 font-mono text-3xl">Hari Om</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
