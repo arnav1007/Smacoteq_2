c658b251f857edadd29946a14e93984cb9b6b41b
16gsanket <16gsanket@gmail.com>
2024-06-06 00:10:30 +0530

    minor fixes by prettierrc file in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  Routes,
-  Route,
-  Navigate,
-  useLocation,
-} from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import "./App.css";
 import Page1 from "./Pages/Page1";
 import Page2 from "./Pages/Page2";
@@ -13,41 +8,36 @@ import PageLayout from "./Ui/PageLayout";
 import { useEffect, useState } from "react";
 
 const routeMapping = {
-  '/ARTH.BHUMI': 1,
-  '/ARTH.BHUMI/WhatWeDo': 2,
+  "/ARTH.BHUMI": 1,
+  "/ARTH.BHUMI/WhatWeDo": 2,
   // Add other routes and their corresponding numbers here
 };
 
 function App() {
-
   const [currentRouteNumber, setCurrentRouteNumber] = useState(null);
   const location = useLocation();
-  
 
   useEffect(() => {
     const number = routeMapping[location.pathname] || null;
-    setCurrentRouteNumber((num) =>num = number);
-
+    setCurrentRouteNumber((num) => (num = number));
   }, [location]);
 
   return (
-      <AppLayout currectRouteNumber={currentRouteNumber}>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/ARTH.BHUMI" />} />
-          <Route
-            path="/ARTH.BHUMI"
-            element={
-              <PageLayout>
-                <Page1 />
-                <Page2/>
-              </PageLayout>
-            }
-          />
-          <Route path="/ARTH.BHUMI/WhatWeDo" element={<Page3 />}>
-            
-          </Route>
-        </Routes>
-      </AppLayout>
+    <AppLayout currectRouteNumber={currentRouteNumber}>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/ARTH.BHUMI" />} />
+        <Route
+          path="/ARTH.BHUMI"
+          element={
+            <PageLayout>
+              <Page1 />
+              <Page2 />
+            </PageLayout>
+          }
+        />
+        <Route path="/ARTH.BHUMI/WhatWeDo" element={<Page3 />}></Route>
+      </Routes>
+    </AppLayout>
   );
 }
 
