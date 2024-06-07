8fb02618ca6e4e36daa90331528ec936441fd286
unknown <arnavgupta9415.ag@gmail.com>
2024-06-07 14:22:26 +0530

    Buttons repaired

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,22 +3,21 @@ import Burger from "../Ui/Burger";
 import Logo from "../Ui/Logo";
 import { NavLink } from "react-router-dom";
 
-function Header({currectRouteNumber}) {
+function Header({ currectRouteNumber }) {
 
-  console.log('currectRouteNumber form Header' , currectRouteNumber)
+  console.log('currectRouteNumber form Header', currectRouteNumber)
   return (
     <div
-      className={`fixed top-0 z-10 flex h-[10dvh] w-full items-center justify-between ${currectRouteNumber===2 ? 'bg-white' : 'bg-main'} px-12 align-middle`}
+      className={`fixed top-0 z-10 flex h-[10dvh] w-full items-center justify-between ${currectRouteNumber === 2 ? 'bg-white' : 'bg-main'} px-12 align-middle`}
     >
       <NavLink to="/">
-
-      <Logo currectRouteNumber={currectRouteNumber}/>
+        <Logo currectRouteNumber={currectRouteNumber} />
       </NavLink>
-      <div className="flex space-x-4 ml-auto mr-40 gap-20">
+      <div className="flex items-center gap-[30px] ml-auto">
         <NavLinkButton currectRouteNumber={currectRouteNumber} to="/" text="VISION" />
         <NavLinkButton currectRouteNumber={currectRouteNumber} to="/ARTH.BHUMI/WhatWeDo" text="ABOUT US" />
+        <Burger currectRouteNumber={currectRouteNumber} />
       </div>
-      <Burger currectRouteNumber={currectRouteNumber}/>
     </div>
   );
 }
