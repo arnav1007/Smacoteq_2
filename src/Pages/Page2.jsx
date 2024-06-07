7ebd9dcd7988811736c3dd7de31bebe4cdc00487
16gsanket <16gsanket@gmail.com>
2024-06-07 14:59:29 +0530

    merge u1 --> main and minor fixes..

--- src/Pages/Page2.jsx
+++ src/Pages/Page2.jsx
@@ -1,30 +1,30 @@
-
-
 function Page2() {
-    return (
-        <div className=" h-fit flex pl-12 align-start justify-center items-start flex-col mb-4">
-        {/* Main text goes */}
-        <h3 className="text-light_text font-ibarra mb-5">
-            Discover the future of international commerce with us. Our mission:
-            to revolutionize global trade using advanced digital technologies.
-            With innovative solutions like Antelope Fi and Arth Bhumi, we're
-            creating a seamless, secure and sustainable ecosystem for trade
-            finance and International Trade Transactions. <br></br>Welcome to
-            Smacoteq- Where GLoabel Trade Evolves
-          </h3>
+  return (
+    <div className="align-start mb-4 flex h-fit flex-col items-start justify-center pl-12">
+      {/* Main text goes */}
+      <h3 className="mb-5 font-ibarra text-light_text">
+        Discover the future of international commerce with us. Our mission: to
+        revolutionize global trade using advanced digital technologies. With
+        innovative solutions like Antelope Fi and Arth Bhumi, we're creating a
+        seamless, secure and sustainable ecosystem for trade finance and
+        International Trade Transactions. <br></br>Welcome to Smacoteq- Where
+        GLoabel Trade Evolves
+      </h3>
 
-          {/* <h3 className="text-light_text font-ibarra mb-5">
+      {/* <h3 className="text-light_text font-ibarra mb-5">
             Scroll Down and below displaying 3 services (Column format) with Images description/icon and button to redirect the user to the platform
           </h3> */}
 
-          <ul className="text-light_text font-ibarra mb-5">
-            <li>1. Smacoteq eBL Services (Button stating Click here redirecting to ebl-smacoteq.com)</li>
-            <li>2. Arth Bhumi (Button stating launching soon)</li>
-            <li>3. AntelopeFi (Button stating Launching soon)</li>
-
-          </ul>
-      </div>
-    )
+      <ul className="mb-5 font-ibarra text-light_text">
+        <li>
+          1. Smacoteq eBL Services (Button stating Click here redirecting to
+          ebl-smacoteq.com)
+        </li>
+        <li>2. Arth Bhumi (Button stating launching soon)</li>
+        <li>3. AntelopeFi (Button stating Launching soon)</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Page2
+export default Page2;
