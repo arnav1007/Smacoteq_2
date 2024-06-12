db262464ca1bac593154734ba56a89d3bd40613d
unknown <arnavgupta9415.ag@gmail.com>
2024-06-13 01:25:52 +0530

    PageX redesigned

--- src/Pages/PageX.jsx
+++ src/Pages/PageX.jsx
@@ -5,7 +5,7 @@ import Maritime from "../Ui/Maritime";
 
 function PageX() {
   return (
-    <div className="relative h-[100dvh] w-screen overflow-y-hidden bg-page3 pt-[10vh]">
+    <div className="relative h-[100dvh] w-screen overflow-hidden bg-page3 pt-[10vh]">
       <div className="absolute bottom-1 right-5 h-[300px] w-[90px]">
         <Maritime text="BLOCKCHAIN" />
       </div>
@@ -13,56 +13,36 @@ function PageX() {
         <div className="my-auto grid h-full w-4/12 place-content-center">
           <img src="/Character.png" alt="" />
         </div>
-        <div className="h-full w-8/12 overflow-y-auto pr-20 pt-[7dvh] font-ibarra text-light_text_page3">
-          <h1 className="my-3 text-2xl text-main">
-            eBL-Electronic Bill of Landing Service
+        <div className="h-full w-6/12 pr-20 pt-[7dvh] font-ibarra text-light_text_page3" style={{ fontSize: "14px" }}>
+          <h1 className="my-1 text-main" style={{ fontFamily: "Ibbara Real Nova", fontSize: "34px" }}>
+            ARTH.BHUMI
           </h1>
-          <div>
+          <h2 className="my-3 " style={{ fontFamily: "Ibbara Real Nova", fontSize: "20px", color: "black" }}>
+            Feature and Services
+          </h2>
+          <div style={{ fontFamily: "Ibbara Real Nova", fontSize: "15px", color: "black" }}>
             <p>
-              Our eBL (electronic Bill of Landing) revolutionize the management
-              of trade documents by digitizing and securing them with
-              cutting-edge blockchain technology. By leveraging a public
-              blockchain and Decentralized IPFS (InterPlanetary File System),
-              all trade documents and images are stored on a decentralized file
-              system. This approach guarantees that every document is
-              tamper-proff, easily verifiable, and immutable. securely
-              transferable and permenanetly stored, enhancing the integrity and
-              efficiency of trade documentations
-            </p>
-          </div>
-          <p className="my-5">Key Features</p>
-          <div>
-            <p>
-              BlockChain Security: Ensuring tamperproof transaction and document
-              integrity.
+              ARTH strives to bring in efficiency & surplus benefits to businesses optimizing
             </p>
             <p>
-              Decentralized IPFS Storage: Enhancing data security by storing
-              documents on a decentralized Inter Planetary file system.
-            </p>
-            <p>
-              Real-TimeNotification :Keeping users informed about the documents
-              status and required actions.
-            </p>
-            <p>
-              Seamless Integration: Compatible with existing ERP and logistics
-              systems for smooth operations.
+              supply chains leveraging technologies at our disposal in the best way possible.
             </p>
           </div>
 
-          <p className="my-5">
-            Smacoteq aims to make this innovative solution accesible not only to
-            large corporations but also to SMEs enhancing efficienct for
-            business of all sizes
-          </p>
-
-          <p className="my-5">
-            <Button type={"small"} to={"https://www.google.com/"}>
-              LAUNCH APP
-            </Button>
-            {/* Button stating LAUNCH APP(ebl.smacoteq.com)
-            (https://ebl.smacoteq.com)-Redirecting to the platform */}
-          </p>
+          <div className="mx-20 mt-8 class1" style={{ fontSize: "12", fontFamily: "Roboto" }}>
+            <p >
+              <span style={{ color: "black" }}>Commodity Tokenization:</span> Quality assured commodities are <span style={{ color: "black" }}>Tokenized on the Polygon </span>Blockchain network. Ownership of these commodities is symbolized by NFTs on the Blockchain.
+            </p>
+            <p>
+              <span style={{ color: "black" }}>Electronic Bill of Lading:</span> Ensure secure and reliable trade with our digitized system that<span style={{ color: "black" }}> lowers overhead cost </span>of paper and reduces unnecessary demurrage and detention charges of holding cargo due to delay in documentation.
+            </p>
+            <p>
+              <span style={{ color: "black" }}>Trade Finance Facilitation:</span> Import and Export Finance through <span style={{ color: "black" }}>AntelopeFi.xyz </span>which makes it possible through systems built on top of <span style={{ color: "black" }}> De-Fi </span>(Decentralized finance) Protocols.
+            </p>
+            <p>
+              <span style={{ color: "black" }}>Cross-border Payment Settlements:</span> Make International Financial transactions seamless without hefty fees with our inclusive and efficient payment system.
+            </p>
+          </div>
         </div>
       </div>
       <div className="h-[10dvh] w-7/12">
